fd4bb48b91982297a5b0ccc43e844d645d0297e7
Toodths <65120698+Toodths@users.noreply.github.com>
2024-02-25 01:22:05 +0000

    update 4.7

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function updateVol(){
   var slider = document.getElementById("myRange");
-  for (let i = 1; i < 5; i++) {
+  for (let i = 1; i < 8; i++) {
     var y = document.getElementById("v" + i.toString())
     y.volume = (slider.value / 100);
   }
@@ -20,4 +20,9 @@ function scramble() {
       x.style.left = xrand.toString() + "px";
       x.style.top = yrand.toString() + "px";
     }
+}
+
+function boarderChange(element){
+  if (!element.paused) { element.style.boxShadow = "0px 0px 40px #9B4F96";}
+  else  { element.style.boxShadow = "0px 0px 3px #9B4F96"; }
 }
