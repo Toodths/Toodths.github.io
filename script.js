6494a73082f558b8ae98460411083732d307aa1a
Toodths <65120698+Toodths@users.noreply.github.com>
2024-02-26 17:34:49 +0000

    5!!!!

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var totalPlayTime = 0;
+const accId = "76561198112693639"
+fetch('https://corsproxy.io/?https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=28FE417B6906C45F07E57AE3177BCFC1&skip_unvetted_apps=false&steamid='+accId+'&format=json&include_played_free_games=1include_appinfo')
+.then(data => data.json())
+.then(Response => {console.log(Response);
+  for (let i = 0; i < Response.response.game_count; i++)
+  {
+    totalPlayTime += Response.response.games[i].playtime_forever;
+  }
+  console.log(totalPlayTime/60);
+  })
+
 function updateVol(){
   var slider = document.getElementById("myRange");
   for (let i = 1; i < 16; i++) {
