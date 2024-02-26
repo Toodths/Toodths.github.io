41f3b9be76699dc649c02bbf7c8c3b48e860eda8
Toodths <65120698+Toodths@users.noreply.github.com>
2024-02-26 02:28:20 +0000

    4.9

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function randomIntFromInterval(min, max) { // min and max included
 function scramble() {
     for (let i = 1; i < 16; i++) {
       var x = document.getElementById(i);
-      var xrand = randomIntFromInterval(675, 1000);
-      var yrand = randomIntFromInterval(400, 880);
+      var xrand = randomIntFromInterval(790, 1010);
+      var yrand = randomIntFromInterval(530, 1385);
       x.style.left = xrand.toString() + "px";
       x.style.top = yrand.toString() + "px";
     }
